246e78c2a7ea2873936f89834ba4d2d354d48613
Olga Firsova <olga.firsova@simbirsoft.com>
2017-03-30 18:20:33 +0400

    add some classes

--- vit/vit/Program.cs
+++ vit/vit/Program.cs
@@ -18,4 +18,39 @@ namespace vit
             Application.Run(new Form1());
         }
     }
+
+    public enum CellType {
+        Empty,
+        Player,
+        Enemy
+    }
+
+    public class Cell
+    {
+        public int row { get; set; }
+        public int col { get; set; }
+        public int weight { get; set; }
+        public CellType type { get; set; }
+
+        public Cell(int row, int col, int weight) {
+            this.row = row;
+            this.col = col;
+            this.weight = weight;
+            this.type = CellType.Empty;
+        }
+
+        public Cell(int row, int col, int weight, CellType type) : this(row, col, weight){
+            this.type = type;
+        }
+    }
+
+    static class Stats {
+        public static int EnemySumWeight { get; set; }
+        public static int EnemyCount { get; set; }
+
+        static Stats() {
+            EnemySumWeight = 1;
+            EnemyCount = 1;
+        }
+    }
 }
